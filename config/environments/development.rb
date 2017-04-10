2882ee5228f1d43cb1db559e86c95c6d07097fe5
samkahchiin <samkahchiin@gmail.com>
2017-04-10 22:27:27 +0800

    [Hotfix] Fixed paperclip s3 loading image issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
