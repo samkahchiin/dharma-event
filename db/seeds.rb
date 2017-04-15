b24dba70dea1ccf63546fe85898c56d8f7303c81
samkahchiin <samkahchiin@gmail.com>
2017-04-15 17:22:47 +0800

    WIP - Edited seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 title = "《心经》与现代生活"
-start_time = Date.new(2017, 5, 27)
-end_time = Date.new(2017, 5, 28)
 speaker = "继持法师"
 description = "幸福人生最重要的条件是内心没有烦恼，没有烦恼时，不论什么情境中都容易开心；烦恼时，碰上再好的事也开心不起。智慧就是帮助我们解除烦恼的一种能力，认识及培养生命内在的自觉的力量是我们改变生命的唯一出路。
 《心经》虽只有短短260个字，但涵义却非常丰富，是一部遇浅即浅，逢深则深的好经！把握一次与《心经》零距离接触的机会，学习观照生命的本质，掀开生命喜悦的篇章！"
 contact = "叶志文 017-6776106"
 location = "冼都斯里兰卡佛寺"
 price = 40
-
 5.times do |event|
-  Event.create(title: title, start_time: start_time, end_time: end_time, speaker: speaker, description: description, contact: contact, location: location, price: price)
+  month = rand(12)
+  area = Event::AREA.sample
+  language = Event::LANGUAGE.sample
+  start_time = Date.new(2017, month, 27)
+  end_time = Date.new(2017, month, 28)
+
+  Event.create(title: title, start_time: start_time, end_time: end_time, speaker: speaker, description: description, contact: contact, location: location, price: price, area: area, language: language)
 end
