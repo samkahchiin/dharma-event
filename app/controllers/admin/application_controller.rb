4a8d6d8372a7d86f526fe592b1b3b3d43f2f446d
samkahchiin <samkahchiin@gmail.com>
2017-05-06 16:23:14 +0800

    WIP - Added authenticate logic

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,7 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to root_url, alert: "You are not authorized" unless current_user.try(:admin)
     end
 
     # Override this value to specify the number of elements to display at a time
