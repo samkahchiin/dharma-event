592c4a53a9d17f78de9303c143f050f4fef7e0c3
samkahchiin <samkahchiin@gmail.com>
2017-05-18 21:35:53 +0800

    WIP - Fixed deprecated message

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_filter :require_permission, only: [:edit, :update, :destroy]
+  before_action :require_permission, only: [:edit, :update, :destroy]
   # GET /events
   # GET /events.json
   def index
