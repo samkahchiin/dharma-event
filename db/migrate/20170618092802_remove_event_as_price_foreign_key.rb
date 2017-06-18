5b18c1f34e3bca9a053501ebe8a03461ceb182bc
samkahchiin <samkahchiin@gmail.com>
2017-06-18 17:29:52 +0800

    WIP - Removed foreign key in prices

---
+++ db/migrate/20170618092802_remove_event_as_price_foreign_key.rb
@@ -0,0 +1,5 @@
+class RemoveEventAsPriceForeignKey < ActiveRecord::Migration[5.0]
+  def change
+    remove_foreign_key :prices, :events
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617150021) do
+ActiveRecord::Schema.define(version: 20170618092802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,5 +70,4 @@ ActiveRecord::Schema.define(version: 20170617150021) do
   end
 
   add_foreign_key "events", "users"
-  add_foreign_key "prices", "events"
 end
