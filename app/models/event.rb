9c9ee5a6fdbcd750a3770d0db62b04e4b6f4fcb6
samkahchiin <samkahchiin@gmail.com>
2017-06-18 13:20:28 +0800

    WIP - Updated event model and spec

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,30 +1,17 @@
 class Event < ApplicationRecord
-  scope :approved, -> { where(status: "approved") }
   belongs_to :user
   has_many :prices
-  accepts_nested_attributes_for :prices, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :prices,
+    reject_if: :all_blank,
+    allow_destroy: true
 
-  include AASM
-  aasm column: "status" do
-    state :pending, initial: true
-    state :approved, :rejected
-
-    event :approve do
-      transitions from: :pending, to: :approved
-      transitions from: :rejected, to: :approved
-    end
-
-    event :reject do
-      transitions from: :pending, to: :rejected
-      transitions from: :approved, to: :rejected
-    end
-  end
+  scope :approved, -> { where(status: "approved") }
 
   STATUS = [
     "pending",
     "approved",
     "rejected"
-  ]
+  ].freeze
 
   AREA = [
     "Johor",
@@ -43,20 +30,29 @@ class Event < ApplicationRecord
     "Selangor",
     "Singapore",
     "Terengganu"
-  ]
-  LANGUAGE = [ "Mandarin" , "English" ]
+  ].freeze
+
+  LANGUAGE = [ "Mandarin" , "English" ].freeze
+
+  validates :status, inclusion: STATUS
+  validates :area, inclusion: AREA
+  validates :language, inclusion: LANGUAGE
+  validates :title, :start_time, :end_time,
+    :speaker, :description, :contact, :location,
+    :organizer_name, :area, :language, :status, presence: true
+
   VALID_IMAGE_CONTENT_TYPE = [
     "image/jpg",
     "image/jpeg",
     "image/png",
     "image/gif"
-  ]
+  ].freeze
 
   VALID_REGISTER_FORM_CONTENT_TYPE = [
     "application/pdf",
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "application/msword"
-  ]
+  ].freeze
 
   has_attached_file :image, styles: {
     thumb: "268x380#"
@@ -65,6 +61,24 @@ class Event < ApplicationRecord
 
   has_attached_file :register_form, default_url: "missing.jpeg"
   validates_attachment_content_type :register_form, content_type: VALID_REGISTER_FORM_CONTENT_TYPE
+
+
+  include AASM
+  aasm column: "status" do
+    state :pending, initial: true
+    state :approved, :rejected
+
+    event :approve do
+      transitions from: :pending, to: :approved
+      transitions from: :rejected, to: :approved
+    end
+
+    event :reject do
+      transitions from: :pending, to: :rejected
+      transitions from: :approved, to: :rejected
+    end
+  end
+
 
   def approved?
     status == "approved"
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:prices) }
+  it { is_expected.to accept_nested_attributes_for(:prices).allow_destroy(true) }
+  it { is_expected.to validate_inclusion_of(:status).in_array(Event::STATUS) }
+  it { is_expected.to validate_inclusion_of(:area).in_array(Event::AREA) }
+  it { is_expected.to validate_inclusion_of(:language).in_array(Event::LANGUAGE) }
+
+  it { is_expected.to validate_presence_of(:language) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:start_time) }
+  it { is_expected.to validate_presence_of(:end_time) }
+  it { is_expected.to validate_presence_of(:speaker) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:contact) }
+  it { is_expected.to validate_presence_of(:location) }
+  it { is_expected.to validate_presence_of(:area) }
+  it { is_expected.to validate_presence_of(:organizer_name) }
+  it { is_expected.to validate_presence_of(:language) }
+  it { is_expected.to validate_presence_of(:status) }
 end
