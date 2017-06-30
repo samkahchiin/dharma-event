a64a732465fefd39ff8a033a6fb0d2c05b54365d
samkahchiin <samkahchiin@gmail.com>
2017-06-30 23:52:00 +0800

    WIP - Run all when everything is filtered out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,14 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
+  # NOTE: This is to make the output more meaningful
   config.formatter = :documentation
+
+  # NOTE: This is to run all specs when everything is filtered out
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
