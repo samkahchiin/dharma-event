e294c692ba8bd4b694b4401e3386e86ea92ab633
samkahchiin <samkahchiin@gmail.com>
2017-07-16 20:37:34 +0800

    WIP - Added feature spec

---
+++ spec/features/event_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+feature "event", js: true do
+  describe "index" do
+    it "should display all approved events" do
+      visit root_path
+      debugger
+    end
+  end
+end
