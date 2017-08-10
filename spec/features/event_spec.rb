79155ad11da9b5f51ed6dd0a57b7073ab70d1769
samkahchiin <samkahchiin@gmail.com>
2017-08-10 22:55:40 +0800

    WIP - Updated show spec

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -1,30 +1,31 @@
 require "rails_helper"
 
 feature "event", js: true, type: :feature do
+  let(:event1) do
+    create :event, :approved,
+          start_time: Date.today + 1.day
+  end
+
+  let(:event2)  do
+    create :event, :approved,
+          start_time: Date.today + 1.year,
+          end_time: Date.today + 370.days
+  end
+
+  let!(:approved_events) do
+    events = create_list :event, 3, :approved
+    events << event1 << event2
+  end
+
+  let!(:pending_events)  { create_list :event, 5 }
+  let!(:rejected_events) { create_list :event, 5, :rejected }
+
+  let(:home_page) do
+    visit root_path
+    HomePage.new
+  end
+
   describe "index" do
-    let(:event1) do
-      create :event, :approved,
-            start_time: Date.today + 1.day
-    end
-
-    let(:event2)  do
-      create :event, :approved,
-            start_time: Date.today + 1.year,
-            end_time: Date.today + 370.days
-    end
-
-    let!(:approved_events) do
-      events = create_list :event, 3, :approved
-      events << event1 << event2
-    end
-
-    let!(:pending_events)  { create_list :event, 5 }
-    let!(:rejected_events) { create_list :event, 5, :rejected }
-    let(:home_page) do
-      visit root_path
-      HomePage.new
-    end
-
     before do
       home_page
     end
@@ -42,6 +43,41 @@ feature "event", js: true, type: :feature do
       last_event = events.last
       expect(last_event.start_time.text).to eq event2.start_time.strftime("%a, %d %b %Y, %R %P")
       expect(last_event.location.text).to eq event2.location
+    end
+  end
+
+  describe "show" do
+    let(:event_show_page) do
+      home_page.events.first.title.click
+      EventShowPage.new
+    end
+
+    before do
+      event_show_page
+    end
+
+    it "should display all the information" do
+      expect(event_show_page.organizer.text).to eq "by " + event1.organizer_name
+
+      [
+        :title,
+        :description,
+        :speaker,
+        :language,
+        :contact,
+        :area
+      ].each do |attr|
+        expect(event_show_page.send(attr).text).to eq event1.send(attr)
+      end
+
+      expect(event_show_page.detailed_start_time.text).to eq event1.start_time.strftime("%F %R %P")
+      expect(event_show_page.detailed_end_time.text).to eq event1.end_time.strftime("%F %R %P")
+
+      prices = event_show_page.prices
+      expect(prices.first.price_type.text).to eq event1.prices.second.price_type
+      expect(prices.first.amount.text).to eq event1.prices.second.amount.to_s
+      expect(prices.second.price_type.text).to eq event1.prices.first.price_type
+      expect(prices.second.amount.text).to eq event1.prices.first.amount.to_s
     end
   end
 end
--- spec/features/support/pages/home_page.rb
+++ spec/features/support/pages/home_page.rb
@@ -10,3 +10,23 @@ class HomePage < SitePrism::Page
 
   sections :events, Event, ".event-card"
 end
+
+class EventShowPage < SitePrism::Page
+  element :title, "#title"
+  element :organizer, "#organizer"
+  element :description, "#description"
+  element :speaker, "#speaker"
+  element :language, "#language"
+  element :contact, "#contact"
+  element :area, "#area"
+  element :start_time, ".general-content-row .start-time"
+  element :detailed_start_time, ".detailed-content-row .start-time"
+  element :detailed_end_time, ".end-time"
+
+  class Price < SitePrism::Section
+    element :amount, ".amount"
+    element :price_type, ".price-type"
+  end
+
+  sections :prices, Price, ".price"
+end
